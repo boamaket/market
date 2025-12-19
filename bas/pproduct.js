10fad2835643ed5d4a2690b03b21c90e30197d35
boamaket <fellixavery@gmail.com>
2025-12-19 10:00:31 +0100

    Add files via upload

---
+++ bas/pproduct.js
@@ -0,0 +1,15 @@
+// PRODUCTS DATA
+const productsData = [
+{
+  id: "P1765562740044",
+  name: "Plenty TikTok account",
+  description: "Plenty TikTok account",
+  price: "10000",
+  currency: "NGN",
+  image: "https://i.ibb.co/PZYVGVq0/Screenshot-20251211-125529.png",
+  sellerId: 6976365864,
+  sellerContact: "+2348121697423",
+  url: "https://i.ibb.co/PZYVGVq0/Screenshot-20251211-125529.png"
+},
+{ id:"P176399696094362", name:"Facebook", description:"Good & Active.", price:"7000", currency:"NGN", image:"https://www.facebook.com/profile.php?id=61582947623474", sellerId: 8111362786, sellerContact: "Whatsapp: +2349166314650", url:"https://www.facebook.com/profile.php?id=61582947623474" },
+];
